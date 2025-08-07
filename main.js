baaff127ef0d8cd1ac5c1cab8dae7971b7741d97
MatiXtreme762 <bilovus.matheus@escola.pr.gov.br>
2025-08-07 07:43:44 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 
+    const palpites = document.querySelector('.palpites');
+    const ultimoResultado = document.querySelector('.ultimoResultado');
+    const baixoOuAlto = document.querySelector('.baixoOuAlto');
+    const envioPalpite = document.querySelector('.envioPalpite');
+    const campoPalpite = document.querySelector('.campoPalpite');
+
+
+
+    function verificarPalpite() {
+        const palpiteUsuario = Number(campoPalpite.value);
+        if (contagemPalpites === 1) {
+            palpites.textContent = "Papites anteriores: ";
+        }
+        
+    palpites.textContent += palpiteUsuario + " ";
+
+    if (palpiteUsuario === numeroAleatorio) {
+        ultimoResultado.textContent = "Parabens! Você Acertou!";
+        
+    }
+    }
